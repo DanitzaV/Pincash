ca71155f9562462409584f1819674d97287c8d01
Cote <majovardon@gmail.com>
2018-10-04 17:32:49 -0400

    Modal reclamo agregado

--- src/js/app.js
+++ src/js/app.js
@@ -41,7 +41,7 @@ displayLogin = () => {
         <p class="noAcc">¿No tienes cuenta?</p>
         <button type="button" class="btn regist mb-1" data-toggle="modal" data-target="#signUp">Regístrate</button>
         <div class="modal fade" id="signUp" tabindex="-1" role="dialog" aria-labelledby="signUpLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
+            <div class="modal-dialog entrada" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title" id="signUpLabel">Ingresa tus datos</h5>
@@ -59,7 +59,7 @@ displayLogin = () => {
                                 <label for="signUpPassword">Contraseña</label>
                                 <input type="password" class="form-control" id="signUpPassword" placeholder="Password">
                             </div>
-                            <small class="form-text text-muted" id="conditions">Registrándote, automáticamente aceptas los <a onclick="termsAndConditions()">Términos y condiciones.</a></small>
+                            <small class="form-text text-muted" id="conditions">Registrándote, automáticamente aceptas los <a onclick="termsAndConditions()" data-dismiss="modal">Términos y condiciones.</a></small>
                         </form>
                     </div>
                     <div class="modal-footer">
@@ -73,13 +73,13 @@ displayLogin = () => {
             <div class="col-12">
                 <button type="button" class="btn mailLogin" data-toggle="modal" data-target="#signIn">COMIENZA</button>
                 <div class="modal fade" id="signIn" tabindex="-1" role="dialog" aria-labelledby="signInLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
+                    <div class="modal-dialog entrada" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title" id="signInLabel">Ingresa tus datos</h5>
                                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true" id="closingX2">&times;</span>
-                                    </button>
+                                </button>
                             </div>
                             <div class="modal-body">
                                 <form>
@@ -122,35 +122,132 @@ displayLogin = () => {
 displayMap = () => {
     data.innerHTML = `
 <div class="container-fluid transition">
-<div class="row cash">
-  <div class="col-2 p-0">
-    <i class="fas fa-bell"></i>
-  </div>
-  <div class="col-10">
-    <p class="firstTitle p-4 m-0">LUGARES CON EFECTIVO</p>
-  </div>
+    <div class="row cash">
+        <div class="col-2 p-0">
+            <i class="fas fa-bell"></i>
+        </div>
+        <div class="col-10">
+            <p class="firstTitle p-4 m-0">LUGARES CON EFECTIVO</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 p-0">
+            <div id="map" style="width: 100%; height: 493px; background: grey"></div>
+                        
+        </div>
+    </div>
+    <div class="row footer text-center fixed-bottom">
+        <div class="col-5 p-0">  
+            <i class="fas fa-location-arrow"></i>
+            <h6 class="cajero-texto">Cajero cercano</h6>  
+        </div>
+        <div class="col-2 p-0">
+            <button type="button" class="btnFooter rounded-circle shadow p-3 bg-white rounded border border-white"><i class="fas fa-sync-alt"></i></button>
+        </div>
+        <div class="col-5 p-0">   
+        <button type="button" class="btn reportBtn" data-toggle="modal" data-target="#myModal"><i class="fas fa-file-invoice showForm "></i></button>
+            <h6 class="reportar-texto ">Reportar</h6>
+        </div>
+    </div>    
 </div>
-<div class="row">
-  <div class="col-12">
-    <div id="mapa"></div>
-  </div>
-</div>
-<div class="row" id="footer">
-  <div class="col-12 text-center">
-    <button type="button" class="btn btn-footer p-4" onclick="logOut()">Salir</button>
-  </div>
-</div>
-</div>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="myModalLabel">Cajero</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col text-center">
+                                <h6 class="prueba text-center">Ayudanos a mejorar la experiencia de este cajero</h6>
+                                <select class="custom-select">
+                                    <option selected>Reportar</option>
+                                    <option value="1">Fuera de Servicio</option>
+                                    <option value="2">Sin dinero</option>
+                                    <option value="3">Cajero cerrado</option>
+                                    <option value="4">No funciona el teclado</option>
+                                    <option value="5">No ingresa tarjeta</option>
+                                </select>
+                                <h6 class="text-center">Otro reporte :</h6>
+                                <textarea class='message' placeholder='Tu reporte'></textarea>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col p-0">
+                                <video id="video"></video>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12 offset-5">
+                                <a id="startbutton"><i class="fas fa-camera"></i></a>
+                            </div>
+                        </div>
+                        <div class="row text-center">
+                            <div class="col-md-12 col-12 p-0">
+                                <canvas id="canvas"></canvas>
+                            </div>
+                            <button class="button button--success btnsu" data-for="js_success-popup">Enviar</button>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="popup popup--icon -success js_success-popup">
+                    <div class="popup__background"></div>
+                    <div class="popup__content">
+                        <h3 class="popup__content__title">
+                            Su reclamo ha sido enviado
+                        </h3>
+                        <p>Gracias a ti, podemos seguir mejorando!</p>
+                        <button class="button button--success" data-for="js_success-popup" onclick="cerrar()">Cerrar</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
 `;
 };
 
 termsAndConditions = () => {
     data.innerHTML = `
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
+<div class="container-fluid transition">
+    <div class="row cash">
+        <div class="col-12 text-center">
+        <h5 class="text-center mt-5 ml-3 mr-3">¡Bienvenido!</h5>
+        <p class="terms text-justify mt-3 ml-3 mr-3 mb-3">
+        Estás a punto de unirte a la primera a la primer red de cajeros y locales comerciales con efectivo en Chile, Pincash es 50% generado por nosotros y el otro 50% por los usuarios. Necesitamos de su colaboración para poder ayudarse mutuamente en mejorar los servicios prestados por los bancos y entidades comerciales.
+        <br>
+        <br>
+        Al hacer clic en "Aceptar", confirmas que:
+        <br>
+        <br>
+        1. Ha leído y está de acuerdo con los 'Términos de uso' http://www.pincash.cl/legal/ y 'Política de privacidad' http://www.pincash.cl/legal/privacy/ (la " Acuerdos "). El uso del Servicio está sujeto a los Acuerdos e indica su consentimiento a ellos. Este resumen no pretende sustituirlos. Está destinado únicamente para fines de conveniencia.
+        <br>
+        <br>
+        2. LOS DATOS DE UBICACIÓN PUEDEN NO SER EXACTOS.
+        <br>
+        <br>
+        3. Usted acepta que Pincash reciba desde su dispositivo móvil información detallada sobre la ubicación y la ruta, por ejemplo, en forma de señales de GPS y otra información. Pincash utiliza esta información para ofrecerle el Servicio, para mejorar la calidad del Servicio que ofrece a usted y para todos sus usuarios, y para mejorar la precisión de sus datos de mapeo y navegación. En particular, Pincash utiliza esta ubicación para poder ayudarle a mejorar su ruta y encontrar mejores alternativas para elegir el lugar más cercano a los usuarios y alternativas más rápidas para llegar a su destino.La configuración de registro permite que pueda el usuario acceder acceder a ciertas funciones de los Servicios (por ejemplo, edición de mapas, clasificación, etc, deberá configurar un nombre de usuario.
+        <br>
+        <br>
+        4. Por la presente, usted confirma que posee todos los derechos exclusivos sobre cualquier dato y contenido (el "Contenido") que proporcione al Servicio y puede asignar dichos derechos a la licencia. Usted conserva todos los títulos y derechos del Contenido, pero le otorga a Pincash, Inc. (la "Compañía") una licencia mundial, gratuita, no exclusiva, irrevocable, sublicimible, transferible y perpetua para usar, copiar, distribuir, crear trabajos derivados de, mostrar públicamente, realizar públicamente y explotar de cualquier otra manera el Contenido. Sujeto a lo mencionado anteriormente, la Compañía conserva los títulos y todos los derechos de la base de datos del Servicio que puede utilizar únicamente para fines no comerciales y privados.
+        <br>
+        <br>
+        5. El servicio Pincash se ofrece de forma gratuita, con la esperanza de que lo encuentre útil. Sin embargo, Pincash o sus empleados, directores, accionistas, asesores o cualquier persona en su nombre no serán responsables ante usted o ante un tercero, por cualquier motivo, como resultado del uso del producto o Servicio de la Compañía. Por la presente, libera irrevocablemente todo lo anterior de cualquier responsabilidad de cualquier tipo, por cualquier consecuencia que surja del uso del Cliente o del Servicio, incluyendo cualquier pérdida, pérdida de beneficios, daños a la reputación, honorarios, gastos o daños, directos o indirectos, financieros o no financieros.
+        <br>  
+        <br>      
+        6. Pincash utiliza el mapa de Here,  y su api places para saber la ubicación de locales comerciales y cajeros automáticos alrededor de su ubicación.
+        </p>
+        <button type="button" class="btn btn-back mb-5" onclick="displayLogin()">Volver</button>
         </div>
     </div>
 </div>
 `;
 };
+//<div id="mapa" style="position:absolute; width:49%; height:71vh; background:grey"></div>
+// <div id="panel" style="position:absolute; width:49%; left:51%; height:100%; background:inherit;></div>
--- src/js/nav.js
+++ src/js/nav.js
@@ -2,37 +2,37 @@
 function showForm() {
     $('.overlay').addClass('active');
     $('.slideUpForm').addClass('active');
-  }
-  
-  function hideForm() {
+}
+
+function hideForm() {
     $('.slideUpForm').removeClass('active');
     $('.overlay').removeClass('active');
-    
-  }
-  
-  $('.showForm').click(function () {
+
+}
+
+$('.showForm').click(function() {
     showForm();
-    document.getElementById('slideUp').style.display= 'block';
-  });
-  
-  $('.close').click(function () {
+    document.getElementById('slideUp').style.display = 'block';
+});
+
+$('.close').click(function() {
     hideForm();
-    
-  });
+
+});
 
 
 // Modal Reclamo
-function cerrar(){
-  // document.getElementById('slideUp').style.display= 'none';
-  location = './mapa.html'
+function cerrar() {
+    // document.getElementById('slideUp').style.display= 'none';
+    location = './mapa.html'
 }
 
 const addButtonTrigger = (el) => {
-	el.addEventListener('click', () => {
-		const popupEl = document.querySelector(`.${el.dataset.for}`);
-		popupEl.classList.toggle('popup--visible');
-	});
+    el.addEventListener('click', () => {
+        const popupEl = document.querySelector(`.${el.dataset.for}`);
+        popupEl.classList.toggle('popup--visible');
+    });
 };
 Array.from(document.querySelectorAll('button[data-for]'))
-           .forEach(addButtonTrigger); 
+    .forEach(addButtonTrigger);
 // Close Modal
